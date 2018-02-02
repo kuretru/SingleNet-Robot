f5a39eac2e39a3be21a190435d62544347390eb0
呉真 <kuretru@gmail.com>
2018-02-02 11:21:16 +0800

    Added feature: log to config file

--- client/app/src/main/java/com/kuretru/singlenet/LogHelper.java
+++ client/app/src/main/java/com/kuretru/singlenet/LogHelper.java
@@ -1,5 +1,7 @@
 package com.kuretru.singlenet;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import java.text.SimpleDateFormat;
@@ -7,7 +9,8 @@ import java.util.Date;
 
 public class LogHelper {
 
-    private static final String DEBUG_TAG = "KT-DEBUG";
+    public static final String DEBUG_TAG = "KT-Singlenet-DEBUG";
+    public static final String PREFS_NAME = "config";
 
     //返回当前时间
     public static String getTimeString(){
@@ -16,13 +19,23 @@ public class LogHelper {
 
     //返回指定时间戳的时间
     public static String getTimeString(Long time){
-        SimpleDateFormat formatter = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat formatter = new SimpleDateFormat ("yy.MM.dd HH:mm:ss");
         Date now = new Date(time);
         String datetime = formatter.format(now);
         return datetime;
     }
 
+    //将日志记录到Logcat
     public static void LogD(String message){
         Log.d(DEBUG_TAG, String.format("%1$s %2$s", getTimeString(), message));
+    }
+
+    //将日志记录到配置文件
+    public static void LogFile(Context context, String message){
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
+        String old = settings.getString("log", "");
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("log", old + String.format("%1$s %2$s\n", getTimeString(), message));
+        editor.commit();
     }
 }

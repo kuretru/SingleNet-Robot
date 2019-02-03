d4b83290a78cb851790e3d3118c013fc98c4c2d3
呉真 <kuretru@gmail.com>
2019-02-03 15:21:27 +0800

    新增：字符串帮助类

---
+++ client/Android/app/src/main/java/com/kuretru/android/singlenet/util/StringUtils.java
@@ -0,0 +1,9 @@
+package com.kuretru.android.singlenet.util;
+
+public class StringUtils {
+
+    public static boolean isNullOrEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+
+}

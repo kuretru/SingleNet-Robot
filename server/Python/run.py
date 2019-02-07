6d0621af90a214225a63f75e0e31ec7749751494
呉真 <kuretru@gmail.com>
2019-02-07 15:32:49 +0800

    新增：预定义变量

--- server/Python/run.py
+++ server/Python/run.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-PORT = 8079
-
 import BaseHTTPServer
+
+PORT = 8079  # 闪讯机器人API服务器监听端口
+INTERFACE = 'wan'  # OpenWrt下外网接口的名称
+SECRET = '123456'  # API服务器通信密钥
 
 
 class SingleNetRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -13,8 +15,10 @@ class SingleNetRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     http_server = BaseHTTPServer.HTTPServer(('', PORT), SingleNetRequestHandler)
+    print('Starting SingleNet Robot API Server at port %d' % PORT)
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
         pass
+    print("Stopping SingleNet Robot API Server")
     http_server.server_close()

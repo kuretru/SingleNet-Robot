f50f100b863af395fd478e7ab00947a33b9ac68e
呉真 <kuretru@gmail.com>
2019-02-13 15:10:23 +0800

    新增：预定义API管理器

---
+++ client/Android/app/src/main/java/com/kuretru/android/singlenet/api/ApiManager.java
@@ -0,0 +1,20 @@
+package com.kuretru.android.singlenet.api;
+
+import com.kuretru.android.singlenet.entity.ApiResponse;
+import com.kuretru.android.singlenet.entity.WanOption;
+
+public class ApiManager {
+
+    public ApiResponse getWanOption(){
+        return null;
+    }
+
+    public ApiResponse setWanOption(WanOption wanOption){
+        return null;
+    }
+
+    public ApiResponse connect(){
+        return null;
+    }
+
+}

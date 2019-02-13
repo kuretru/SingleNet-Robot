0ff53a13cd5e5cfa9f9af38c344e524cbbfd1d3c
呉真 <kuretru@gmail.com>
2019-02-13 14:52:52 +0800

    新增：上网账号实体类

---
+++ client/Android/app/src/main/java/com/kuretru/android/singlenet/entity/WanOption.java
@@ -0,0 +1,29 @@
+package com.kuretru.android.singlenet.entity;
+
+public class WanOption {
+
+    private String username;
+
+    private String password;
+
+    public WanOption() {
+        super();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}

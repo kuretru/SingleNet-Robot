7e0fc5f1cedc0d34c16f135ca9e0fa76b4d008db
呉真 <kuretru@gmail.com>
2019-02-17 22:05:46 +0800

    新增：返回JSON格式数据

--- server/Python/run.py
+++ server/Python/run.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import BaseHTTPServer
+import json
 import subprocess
+import time
+from collections import OrderedDict
 
 PORT = 8079  # 闪讯机器人API服务器监听端口
 INTERFACE = 'wan'  # OpenWrt下外网接口的名称
@@ -31,6 +34,25 @@ def set_password(password):
 
 class SingleNetRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
+    def handle_method(self, method):
+        route = self.path.strip('/')
+        if route == '':
+            now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            self.send_content(now)
+        else:
+            self.send_error(404)
+
+    def send_content(self, data):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json;charset=UTF-8')
+        self.end_headers()
+        self.wfile.write(self.api_response(data))
+
+    @staticmethod
+    def api_response(data):
+        response = OrderedDict([('code', 2000), ('message', 'success'), ('data', data)])
+        return json.dumps(response)
+
     def do_GET(self):
         self.handle_method('GET')
 
@@ -42,12 +64,6 @@ class SingleNetRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_DELETE(self):
         self.handle_method('DELETE')
-
-    def handle_method(self, method):
-        route = self.path.strip('/')
-        print(route)
-        print(get_username())
-        self.send_error(404)
 
 
 if __name__ == '__main__':

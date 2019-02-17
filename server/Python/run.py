6e49569c634b0ed85d8a4131ee0976530bbe494a
呉真 <kuretru@gmail.com>
2019-02-17 15:07:20 +0800

    新增：操作uci方法

--- server/Python/run.py
+++ server/Python/run.py
@@ -2,15 +2,52 @@
 # -*- coding: utf-8 -*-
 
 import BaseHTTPServer
+import subprocess
 
 PORT = 8079  # 闪讯机器人API服务器监听端口
 INTERFACE = 'wan'  # OpenWrt下外网接口的名称
 SECRET = '123456'  # API服务器通信密钥
 
 
+def get_username():
+    result = subprocess.check_output(['uci', 'get', 'network.{}.username'.format(INTERFACE)])
+    return result
+
+
+def get_password():
+    result = subprocess.check_output(['uci', 'get', 'network.{}.password'.format(INTERFACE)])
+    return result
+
+
+def set_username(username):
+    subprocess.call(['uci', 'set', 'network.{}.username={}'.format(INTERFACE, username)])
+    return get_username()
+
+
+def set_password(password):
+    subprocess.call(['uci', 'set', 'network.{}.password={}'.format(INTERFACE, password)])
+    return get_password()
+
+
 class SingleNetRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def __init__(self):
-        self.route = self.path.strip('/')
+
+    def do_GET(self):
+        self.handle_method('GET')
+
+    def do_POST(self):
+        self.handle_method('POST')
+
+    def do_PUT(self):
+        self.handle_method('PUT')
+
+    def do_DELETE(self):
+        self.handle_method('DELETE')
+
+    def handle_method(self, method):
+        route = self.path.strip('/')
+        print(route)
+        print(get_username())
+        self.send_error(404)
 
 
 if __name__ == '__main__':

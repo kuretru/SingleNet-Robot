9e2eafbcd15491727f134b2859272f95a24172a0
呉真 <kuretru@gmail.com>
2019-04-20 23:37:07 +0800

    修改：升级Gradle版本

---
+++ client/Android/app/src/main/java/com/kuretru/android/singlenet/activity/SinglenetAppWidget.java
@@ -0,0 +1,37 @@
+package com.kuretru.android.singlenet.activity;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import com.kuretru.android.singlenet.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class SinglenetAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.singlenet_app_widget);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+}
+
